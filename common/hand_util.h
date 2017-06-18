2b57399d2e6fc322186f82af77c962ea247a4cce
Walker Ravina <ravina.walker@gmail.com>
2017-06-18 00:07:52 -0700

    Star making hand_util. Add more to card.proto.

--- common/hand_util.h
+++ common/hand_util.h
@@ -10,6 +10,14 @@
 #include "proto/card.pb.h"
 
 namespace jam {
-    bool compare_cards(jam::Card const& c1, jam::Card const& c2);
+    void assign_hand_type(jam::Hand* hand);
+    bool is_straight_flush(jam::Hand const& hand);
+    bool is_four_of_a_kind(jam::Hand const& hand);
+    bool is_full_house(jam::Hand const& hand);
     bool is_flush(jam::Hand const& hand);
+    bool is_straight(jam::Hand const& hand);
+    bool is_three_of_a_kind(jam::Hand const& hand);
+    bool is_two_pair(jam::Hand const& hand);
+    bool is_high_card(jam::Hand const& hand);
+    std::vector<jam::Card> sorted_hand(jam::Hand const& hand);
 }
