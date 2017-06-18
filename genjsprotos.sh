882928e004df2038f03bf0e81ff36e2fc0b6af04
Patrick Violette <violette.patrick718@gmail.com>
2017-06-17 19:03:21 -0700

    Add some more proto fields.

--- genjsprotos.sh
+++ genjsprotos.sh
@@ -3,5 +3,5 @@
 command -v pbjs 2>&1 > /dev/null || npm install -g protobufjs@6.8.0
 
 # Add a call to factory(protobuf) to get the lib in protobuf.roots.default.jam.
-pbjs proto/card.proto proto/hand_state.proto -t static-module --no-create \
+pbjs proto/card.proto proto/hand_state.proto proto/game_state.proto -t static-module \
 | sed 's%factory(require("protobufjs/minimal"));%1;\nfactory(protobuf);\n%g' > web/proto/jam.proto.js && echo 'Generated protos.'
