4c452705fd460700e1ed2b5a0c57e1ed6f635036
Patrick Violette <violette.patrick718@gmail.com>
2017-10-29 18:03:38 -0700

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,3 +1,8 @@
 #/bin/bash
+# Install proto3
 git clone -b 'v3.3.1' --single-branch --depth=1 https://github.com/google/protobuf.git
 cd protobuf
+sudo apt-get install autoconf automake libtool curl make g++ unzip
+./autogen.sh
+make
+sudo make install
