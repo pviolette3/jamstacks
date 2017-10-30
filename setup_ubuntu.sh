5523cf71824f4a6f2c743aa17e0ecea05ee48eed
Patrick Violette <violette.patrick718@gmail.com>
2017-10-29 18:41:26 -0700

    Add how to install buck

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -6,3 +6,12 @@ sudo apt-get install autoconf automake libtool curl make g++ unzip
 ./autogen.sh
 make
 sudo make install
+
+# Install BUCK
+git clone https://github.com/facebook/buck.git
+sudo apt-get update
+sudo apt-get install default-jdk ant git
+git clone -b 'v2017.10.01.01' --single-branch --depth=1 https://github.com/facebook/buck.git
+cd buck
+ant
+./bin/buck --help
