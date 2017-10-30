94e2faed58d65ee4f5d1e86f8dd43200cd7d7b6c
Daniel MacLennan <daniel@Daniels-MacBook-Pro.local>
2017-10-30 01:23:16 -0700

    protos work

--- server/test.cpp
+++ server/test.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "gflags/gflags.h"
+#include "card.pb.h"
 
-int main() {
-  std::cout << "hello world\n";
+DEFINE_string(foo, "", "A thing");
+
+int main(int argc, char **argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  jam::Card c;
+  c.set_suit(jam::Card::CLUBS);
   return 0;
 }
